983a4660560042e9704aba3be818d8d4a2b2ab2e
Harshita <harshitalkadeep@gmail.com>
2024-08-23 13:34:30 +0530

    add navbar dropdown

--- calculator/src/components/Navbar.js
+++ calculator/src/components/Navbar.js
@@ -1,19 +1,102 @@
 import React from 'react';
-import { Box, Flex, Link } from '@chakra-ui/react';
-import greenSizz from '../images/logo.jpg'; 
+import {
+  Box,
+  Flex,
+  Link,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  IconButton,
+} from '@chakra-ui/react';
+import { ChevronDownIcon } from '@chakra-ui/icons';
+import greenSizz from '../images/logo.jpg';
 
 const Navbar = () => (
   <Box bg="primary.500" px={4} py={2} color="white">
     <Flex h={16} alignItems="center" justifyContent="space-between">
+      {/* Left Side - Logo */}
       <Box display="flex" alignItems="center">
-        <img src={greenSizz} alt="greenSizz" style={{ width: '65px', height: 'auto', marginRight: '30px' }} />
-        <span style={{ fontWeight: 'bold' }}>Health & Fitness Calculators</span>
+        <img
+          src={greenSizz}
+          alt="greenSizz"
+          style={{ width: '65px', height: 'auto', marginRight: '30px' }}
+        />
       </Box>
+
+      {/* Right Side - Menu Items */}
       <Flex alignItems="center">
-        <Link px={2} href="https://www.greensizz.com/">Home</Link>
+        <Menu>
+          <MenuButton
+            as={Link}
+            fontWeight="bold"
+            _hover={{ textDecoration: 'none', color: 'green.200' }}
+            cursor="pointer"
+            display="flex"
+            alignItems="center"
+            mr={8} 
+          >
+            Health & Fitness Calculators <ChevronDownIcon ml={2} />
+          </MenuButton>
+          <MenuList bg="primary.600" borderColor="teal.200" color="green.800">
+            <MenuItem
+              _hover={{ bg: 'green.500', color: 'white' }}
+              onClick={() => window.location.href = '/bmindiw'}
+            >
+              BMI & Ideal Weight Calculator
+            </MenuItem>
+            <MenuItem
+              _hover={{ bg: 'green.500', color: 'white' }}
+              onClick={() => window.location.href = '/dailycalorie'}
+            >
+              Daily Calorie Calculator
+            </MenuItem>
+            <MenuItem
+              _hover={{ bg: 'green.500', color: 'white' }}
+              onClick={() => window.location.href = '/protein'}
+            >
+              Protein Calculator
+            </MenuItem>
+            <MenuItem
+              _hover={{ bg: 'green.500', color: 'white' }}
+              onClick={() => window.location.href = '/food-calorie'}
+            >
+              Food Calorie Calculator
+            </MenuItem>
+            <MenuItem
+              _hover={{ bg: 'green.500', color: 'white' }}
+              onClick={() => window.location.href = '/nutrient'}
+            >
+              Nutrient Calculator
+            </MenuItem>
+            <MenuItem
+              _hover={{ bg: 'green.500', color: 'white' }}
+              onClick={() => window.location.href = '/period'}
+            >
+              Period Calculator
+            </MenuItem>
+            <MenuItem
+              _hover={{ bg: 'green.500', color: 'white' }}
+              onClick={() => window.location.href = '/pregnancy'}
+            >
+              Pregnancy Calculator
+            </MenuItem>
+          </MenuList>
+        </Menu>
+
+        <Link
+          px={2}
+          href="https://www.greensizz.com/"
+          _hover={{ color: 'green.200' }}
+          ml={4} 
+          fontWeight="bold"
+        >
+          Home
+        </Link>
       </Flex>
     </Flex>
   </Box>
 );
 
 export default Navbar;
+
