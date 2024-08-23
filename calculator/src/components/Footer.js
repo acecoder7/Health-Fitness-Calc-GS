61c04ae6fdcc3d2113de40d901d5c78b81a002d6
Harshita <harshitalkadeep@gmail.com>
2024-08-23 13:05:23 +0530

    footer css fixed

--- calculator/src/components/Footer.js
+++ calculator/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Box, Text } from '@chakra-ui/react';
 
 const Footer = () => (
-  <Box bg="primary.500" color="white" py={4} textAlign="center">
+  <Box bg="primary.500" color="white" py={4} textAlign="center" mt={8}>
     <Text>&copy; 2024 GreenSizz Private Limited. All rights reserved.</Text>
   </Box>
 );
