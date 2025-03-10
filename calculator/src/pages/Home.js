498a68baee1e63c1b5e96df15d9a134ce898a6e7
Harshita <harshitalkadeep@gmail.com>
2025-03-10 12:53:53 +0530

    calc icon updated

--- calculator/src/pages/Home.js
+++ calculator/src/pages/Home.js
@@ -2,31 +2,41 @@ import React from "react";
 import { Box, SimpleGrid } from "@chakra-ui/react";
 import MainCard from "../components/MainCard";
 import healthicon from "../images/cardicon.png";
+import bmi from "../images/cicon/bmi.png";
+import boll from "../images/cicon/boll.png";
+import daily from "../images/cicon/daily.png";
+import food from "../images/cicon/food.png";
+import menstr from "../images/cicon/menstr.png";
+import nutri from "../images/cicon/nutri.png";
+import pcos from "../images/cicon/pcos.png";
+import preg from "../images/cicon/preg.png";
+import prot from "../images/cicon/prot.png";
+
 
 const calculators = [
-  { name: "BMI and Ideal Weight Calculator", icon: healthicon, link: "/bmindiw" },
-  { name: "Daily Calorie Calculator", icon: healthicon, link: "/dailycalorie" },
-  { name: "Protein Calculator", icon: healthicon, link: "/protein" },
+  { name: "BMI and Ideal Weight Calculator", icon: bmi, link: "/bmindiw" },
+  { name: "Daily Calorie Calculator", icon: daily, link: "/dailycalorie" },
+  { name: "Protein Calculator", icon: prot, link: "/protein" },
   {
     name: "Food Calorie Calculator",
-    icon: healthicon,
+    icon: food,
     link: "/food-calorie",
   },
-  { name: "Nutrient Calculator", icon: healthicon, link: "/nutrient" },
-  { name: "Period Calculator", icon: healthicon, link: "/period" },
+  { name: "Nutrient Calculator", icon: nutri, link: "/nutrient" },
+  { name: "Period Calculator", icon: menstr, link: "/period" },
   {
     name: "Pregnancy Calculator",
-    icon: healthicon,
+    icon: preg,
     link: "/pregnancy",
   },
   {
     name: "Explore Nutrients Bowls",
-    icon: healthicon,
+    icon: boll,
     link: "/explore-all",
   },
   {
     name: "Explore Ovaboost Care Bowls",
-    icon: healthicon,
+    icon: pcos,
     link: "/explore-pcos",
   },
 ];
